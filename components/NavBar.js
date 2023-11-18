795969b8e53d7eb90cd0069e80ff412a2a60bb9b
Mandeep Singh <92678152+mandeep-s1ngh@users.noreply.github.com>
2023-11-18 16:29:27 +0000

    work on navigation. install framer motion, use it on logo component.

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,15 +1,34 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
+import Logo from "./Logo";
+import { useRouter } from "next/navigation";
+
+const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span
+        className={`h-[1px] inline-block w-full bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === href ? "w-full" : "w-0"
+        }`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
 
 const NavBar = () => {
   return (
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
       <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/projects">Projects</Link>
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/projects" title="Projects" className="mx-4" />
       </nav>
-      <h2>Logo</h2>
       <nav>
         <Link href="/" target={"_blank"}>
           GitHub
@@ -21,6 +40,9 @@ const NavBar = () => {
           Dark Mode
         </Link>
       </nav>
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
     </header>
   );
 };
