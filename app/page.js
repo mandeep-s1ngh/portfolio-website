dc5f98713006d32c5a8ba84c6df3c2a7da5a7766
Mandeep Singh <92678152+mandeep-s1ngh@users.noreply.github.com>
2023-11-19 21:36:18 +0000

    add profile picture to homepage

--- app/page.js
+++ app/page.js
@@ -1,3 +1,21 @@
+import Layout from "@/components/Layout";
+import Image from "next/image";
+import ProfilePic from "../public/images/profile/profile-pic.png";
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main className="flex items-center text-dark w-full min-h-screen">
+      <Layout className="pt-0">
+        <div className="flex items-center justify-between w-full">
+          <div className="w-1/2">
+            <Image
+              src={ProfilePic}
+              alt="Profile picture of Mandeep Singh"
+              className="w-full h-auto"
+            />
+          </div>
+        </div>
+      </Layout>
+    </main>
+  );
 }
